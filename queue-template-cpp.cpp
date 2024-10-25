39cb8a2dd16d85ab92ced37fa4dd6c713788106d
Klimenko George <george_klimenko6@mail.ru>
2024-10-25 21:44:52 +0300

    Initial commit

---
+++ queue-template-cpp.cpp
@@ -0,0 +1,41 @@
+﻿#include "queue-template-cpp.h"
+
+template<typename T, class Container>
+T Queue<T, Container>::front() const {
+	return *data.begin();
+}
+
+template<typename T, class Container>
+T& Queue<T, Container>::front() {
+	return *data.begin();
+}
+
+template<typename T, class Container>
+void Queue<T, Container>::pop() {
+	data.pop_front();
+}
+
+template<typename T, class Container>
+void Queue<T, Container>::push(const T& add) {
+	data.push_back(add);
+}
+
+template<typename T, class Container>
+size_t Queue<T, Container>::size() const {
+	return data.size();
+}
+
+template<typename T, class Container>
+bool Queue<T, Container>::empty() const {
+	return data.empty();
+}
+
+template<typename T, class Container>
+bool Queue<T, Container>::operator==(const Queue& rhs) const {
+	return data == rhs.data;
+}
+
+template<typename T, class Container> 
+bool Queue<T, Container>::operator!=(const Queue& rhs) const {
+	return !(*this == rhs);
+}
---
+++ queue-template-cpp.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <deque>
+
+template <typename T, class Container = std::deque<T>>
+class Queue {
+private:
+	Container data = Container();
+
+public:
+	Queue() = default;
+
+	T front() const;
+	T& front();
+	void pop();
+	void push(const T& add);
+	size_t size() const;
+	bool empty() const;
+	bool operator ==(const Queue& rhs) const;
+	bool operator !=(const Queue& rhs) const;
+};
